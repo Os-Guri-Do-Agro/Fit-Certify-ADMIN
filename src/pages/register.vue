0cbe0c4572f27312cf9d3d49b894b5f72bf68ec3
Luis Gustavo <darkbj13.lg@gmail.com>
2025-08-26 20:59:47 -0300

    fix: ajusta código de registro / apaga arquivos denecessário

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -57,16 +57,169 @@
           class="d-flex flex-column h-100 w-100 justify-space-between"
           :items="item"
         >
+
+
+        <!--Primeiro slid-->
           <template #item.1>
-            <CadastroBasico />
+                    <VRow>
+            <VCol class="mb-10 mt-10 mt-md-0" cols="12"><h2 class="text-start text-h5 font-weight-bold" style="color: #88CE0D;">1. Informações Pessoais</h2></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12" md="8"><label for="nomeCompleto">Nome completo:</label><VTextField density="compact" name="nomeCompleto" id="nomeCompleto" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12" md="4"><label for="cpf">CPF</label><VTextField density="compact" name="cpf" id="cpf" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12" md="4"><label for="telefone">Telefone:</label><VTextField density="compact" name="telefone" id="telefone" type="tel" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12" md="4"><label for="email">E-mail</label><VTextField density="compact" name="email" id="email" type="email" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12" md="4"><label for="nascimento">Data de nascimento:</label><VTextField density="compact" name="nascimento" id="nascimento" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="6" md="12"><label for="altura">Altura (cm):</label><VTextField density="compact" name="altura" id="altura" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="6" md="12"><label for="peso">Peso (kg):</label><VTextField density="compact" name="peso" id="peso" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="telefone">Telefone:</label><VTextField density="compact" name="telefone02" id="telefone02" type="tel" variant="outlined"/></VCol>
+            <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="pratica">Pratica atividade física regularmente?</label><v-select density="compact"
+      v-model="select.value.value"
+      :error-messages="select.errorMessage.value"
+      :items="items"
+      id="pratica"
+      placeholder="Sim"
+      variant="outlined"
+    ></v-select></VCol>
+    <VCol class="w-100" cols="12">
+    </VCol>
+                                      
+        </VRow>
           </template>
 
+
+          <!--Segundo slide-->
           <template #item.2>
-            <CadastroSaude />
+            <VRow class="pa-0 ma-0 fill-md-height d-flex flex-column flex-md-row">
+
+
+            <VCol class="d-flex ma-0 pa-0 pr-0 pr-md-3"  md="6">
+                <v-row class="d-flex " >
+                    <v-col cols="12">
+                <h2 class="text-start text-h5 font-weight-bold mb-5" style="color: #88CE0D;">2. Histórico de Saúde</h2>
+                <div class="mb-5"><span class="text-black font-weight-medium">Já foi diagnosticado com alguma das condições abaixo?</span></div>
+                        <v-checkbox
+                            v-for="(item, index) in condicoes"
+                            :key="index"
+                            v-model="selecionados"
+                            :label="item"
+                            :value="item"
+                            hide-details
+                            density="compact"
+                            color="success"
+                        />
+
+                        <VRow>
+                            <VCol cols="12">
+                        <div class="mt-5 d-flex flex-column">
+                            <span class="text-black">Outras condições médicas:</span>
+                            <v-textarea class="text-black" no-resize rows="2" color="black" variant="outlined"></v-textarea>
+                        </div>
+                            </VCol>
+
+
+                            <VCol cols="12">
+                        <div class="mt-5 d-flex flex-column">
+                            <span class="text-black">Toma algum medicamento contínuo? Se sim, qual?</span>
+                            <v-textarea class="text-black custom-textarea" rows="2"  no-resize max-height="20px" color="black" variant="outlined"></v-textarea>
+                        </div>
+                            </VCol>
+                        </VRow>
+
+                    </v-col>
+                </v-row>
+            </VCol>
+
+            
+            <VCol class="d-flex pa-0 ma-0" md="6">
+                <v-row class="d-flex h-100" >
+                    <v-col class="h-100" cols="12">
+                <h2 class="text-start text-h5 font-weight-bold mb-5" style="color: #88CE0D;">3. Sinais e Sintomas Recentes</h2>
+                <div class="mb-5"><span class="text-black font-weight-medium">Já foi diagnosticado com alguma das condições abaixo?</span></div>
+                        <v-checkbox
+                            v-for="(item, index) in sintomas"
+                            :key="index"
+                            v-model="selecionados"
+                            :label="item"
+                            :value="item"
+                            hide-details
+                            density="compact"
+                            color="success"
+                        />
+
+                        <div class="mt-7">
+                            <h2 class="text-start text-h5 font-weight-bold mb-7" style="color: #88CE0D;">4. Histórico Esportivo</h2>
+                            <VRow>
+                                <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="objetivo">Objetivo com a atividade física:</label><VTextField name="objetivo" id="objetivo"  placeholder="Saúde geral" variant="outlined"/></VCol>
+                                <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="provas">Já participou de provas antes?</label><VTextField name="provas"  id="provas" placeholder="Sim" variant="outlined"/></VCol>
+                                <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="ultimasprovas">Se sim, qual a última?</label><VTextField name="ultimasprovas" id="ultimasprovas" variant="outlined"/></VCol>
+                                
+                            </VRow>
+                        </div>
+
+                    </v-col>
+                </v-row>
+            </VCol>
+
+        </VRow>
           </template>
 
+
+
+          <!--Terceiro slide-->
           <template #item.3>
-            <CadastroSaude02 />
+  <div class="d-flex flex-column justify-space-between">
+    <div class="mt-10 mt-md-0 d-flex">
+                <h2 class="text-start text-h5 font-weight-bold mb-5" style="color: #88CE0D;">
+                    5. Exames e Dados Complementares
+                </h2>
+            </div>
+
+            <VRow class="d-flex">
+                <v-form class="w-100">
+                <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="check">Fez check-up nos últimos 12 meses?</label><v-select
+        density="comfortable"
+      v-model="checkselect.value.value"
+      :error-messages="checkselect.errorMessage.value"
+      :items="items"
+      name="check"
+      id="check"
+      placeholder="Sim"
+      variant="outlined"
+    ></v-select></VCol>
+                <VCol class="my-0 py-0 font-weight-medium" cols="12"><label for="arquivos">Anexar exames (PDF ou imagem):</label><VFileInput density="comfortable" prepend-icon="" name="arquivos" id="arquivos" variant="outlined"/></VCol>
+                <VCol class="my-0 py-0 font-weight-medium " cols="12"><label for="app">Possui smartwatch ou app de treino?</label><v-select
+        density="comfortable"
+      v-model="appselect.value.value"
+      :error-messages="appselect.errorMessage.value"
+      :items="items"
+      name="app"
+      id="app"
+      placeholder="Sim"
+      variant="outlined"
+    ></v-select></VCol>
+            <div class="">
+                <v-checkbox class="font-weight-medium" color="success" label=" Desejo integrar meus dados com a FitCertify365"></v-checkbox>
+            </div>
+            </v-form>
+            </VRow>
+
+
+
+            <div class="d-flex flex-column h-100">
+                <h2 class="text-start text-h5 font-weight-bold mb-5" style="color: #88CE0D;">
+                    6. Declaração
+                </h2>
+                <v-form class="w-100">
+            <v-checkbox
+            v-for="(item, index) in declaracoes"
+            :key="index"
+            v-model="selecionados"
+            :label="item"
+            :value="item"
+            density="compact"
+            class="font-weight-medium" color="success" ></v-checkbox>
+                </v-form>
+            </div>
+            </div>
           </template>
 
          <template #actions="{ next, prev }">
@@ -104,12 +257,34 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import CadastroBasico from '../components/cadastros/cadastroBasico.vue'
 import CadastroSaude02 from '../components/cadastros/cadastroSaude02.vue'
-import CadastroSaude from '../components/cadastros/CadastroSaude.vue'
+import { useField, useForm } from 'vee-validate'
 
 const step = ref(1)
 const router = useRouter()
+
+  const { handleSubmit } = useForm({
+    validationSchema: {
+
+      select (value) {
+        if (value) return true
+
+        return 'Select an item.'
+      },
+    },
+  })
+
+  const select = useField('select')
+ 
+
+  const items = ref([
+    'Sim',
+    'Não'
+  ])
+
+  const submit = handleSubmit(values => {
+    alert(JSON.stringify(values, null, 2))
+  })
 
 
 const handleNext = (next) => {
@@ -120,6 +295,38 @@ const handleNext = (next) => {
     next()
   }
 }
+
+const condicoes = [
+  'Hipertensão',
+  'Diabetes',
+  'Asma',
+  'Colesterol alto',
+  'Problemas cardíacos',
+  'Doenças renais',
+  'Problemas respiratórios',
+  'Nenhuma das anteriores'
+]
+
+const sintomas = [
+    'Dor no peito ao esforço',
+    'Falta de ar com atividades leves',
+    'Tontura ou desmaios',
+    'Palpitações',
+    'Inchaço nas pernas',
+    'Nenhum desses'
+]
+
+const selecionados = ref([])
+
+
+  const checkselect = useField('checkselect')
+  const appselect = useField('appselect')
+  
+  const declaracoes = [
+    'Declaro que as informações acima são verdadeiras e autorizo a análise para fins de certificação.',
+    'Aceito compartilhar meus dados com as organizações dos eventos que eu participar.',
+    'Li e concordo com os Termos de Uso e Política de Privacidade.'
+  ]
 
 const item = [
   'Cadastro Básico',
