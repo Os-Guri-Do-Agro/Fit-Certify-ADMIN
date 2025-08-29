2f07e9ebed530e091c38b246cf0b0f1e695c868b
Marcel <marcel10042002@gmail.com>
2025-08-29 08:26:14 -0400

    fix: ajuste getPayload

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -28,16 +28,24 @@ export const checkAuthAndRedirect = () => {
 }
 
 export const getPayload = () => {
-    const token = sessionStorage.getItem('token')
-
-    if (!token) return false
-
+    const token = sessionStorage.getItem('token');
+  
+    if (!token) return false;
+  
     try {
-        const payload = JSON.parse(atob(token.split('.')[1]))
-
-        return payload
+      const base64 = token.split('.')[1];
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+          .join('')
+      );
+  
+      const payload = JSON.parse(jsonPayload);
+      
+      return payload;
     } catch (error) {
-        console.log(error)
-        return false
+      console.log(error);
+      return false;
     }
-}
+  };
